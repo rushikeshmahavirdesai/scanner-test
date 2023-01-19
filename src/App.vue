7fc8f71b87808c8c2ccb7c97a36fba9de872d0aa
rushikeshmahavirdesai <rushikeshmahavirdesai@gmail.com>
2023-01-19 14:16:54 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <QuoggaReader @onDetected="onDecode" />
+  <!-- <StreamBarcodeReader v-if="cart.mobileScanner" :busy="data.isAdding" @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader> -->
+</template>
+
+<script setup>
+import { ref, reactive } from "vue";
+import StreamBarcodeReader from "./components/StreamBarcodeReader.vue";
+import QuoggaReader from "./components/QuoggaReader.vue";
+
+const data = reactive({ loading: true, isAdding: false });
+const audio = new Audio("/beep.mp3");
+
+const onLoaded = () => (data.loading = false);
+
+const onDecode = async (text) => {
+  if (data.isAdding) return false;
+  alert("text.codeResult.code", text.codeResult.code);
+  text.codeResult.code;
+  data.isAdding = true;
+
+  data.isAdding = false;
+};
+</script>
+
+<style scoped>
+.information {
+  margin-top: 100px;
+}
+</style>
